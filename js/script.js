f21a6f539686d0bfdf6539a31c84693ff882cdc7
Bubbline Bee <duckwhaleempire@gmail.com>
2020-09-07 17:32:41 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,16 +12,17 @@ var bord = new Image();
 
 bord.src = "img/border_black.svg";
 
-userimage.addEventListener("change", redraw);
+userimage.addEventListener("change", imageLoad);
 dpi.addEventListener("change", redraw);
 width.addEventListener("change", redraw);
 height.addEventListener("change", redraw);
 x.addEventListener("change", redraw);
 y.addEventListener("change", redraw);
+fr.addEventListener("load", redraw)
 
 redraw()
 
-async function redraw() {
+function redraw() {
 	var cw = (width.value * 0.03937008 * dpi.value);
 	var ch = (height.value * 0.03937008 * dpi.value);
 	
@@ -33,20 +34,21 @@ async function redraw() {
 	context.ellipse((cw / 2) - 2, (ch / 2) - 2, cw / 2 - 1, ch / 2 - 1, 0, 0, Math.PI * 2);
 	context.fill();
 	
-	if (userimage.value) {
-		context.globalCompositeOperation = "source-atop";
+	context.globalCompositeOperation = "source-atop";
 		
-		await fr.readAsDataURL(userimage.files[0]);
-		if (fr.readyState == 2) {
-			img.src = fr.result;
+	if (fr.readyState == 2) {
+		img.src = fr.result;
 		
-			context.drawImage(img, parseInt(x.value), parseInt(y.value));
-			context.globalCompositeOperation = "source-over";
-		} else {
-			console.log("Load incomplete.");
-		}
+		context.drawImage(img, parseInt(x.value), parseInt(y.value));
+		context.globalCompositeOperation = "source-over";
 	}
 	
 	context.drawImage(bord, 0, 0, cw - 1, ch - 1);
 	return;
+}
+
+function imageLoad() {
+	if (userimage.value) {
+		fr.readAsDataURL(userimage.files[0]);
+	}
 }
