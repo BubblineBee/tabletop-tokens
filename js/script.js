2d6c709c34ef2503be04964c8e267090b9d89988
Bubbline Bee <duckwhaleempire@gmail.com>
2020-09-07 13:24:30 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-var image;
-var dpi;
-var width;
-var height;
-var x;
-var y;
+var userimage = document.getElementById("userimage");;
+var dpi = document.getElementById("dpi");;
+var width = document.getElementById("width");;
+var height = document.getElementById("height");;
+var x = document.getElementById("xoffset");;
+var y = document.getElementById("yoffset");;
+
+userimage.addEventListener("change", redraw);
+dpi.addEventListener("change", redraw);
+width.addEventListener("change", redraw);
+height.addEventListener("change", redraw);
+x.addEventListener("change", redraw);
+y.addEventListener("change", redraw);
+
+function redraw() {
+	canvas.width = width
+	canvas.height = height
+}
